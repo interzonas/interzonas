b651e0be767d2ca37288bcd504d524efac0f34c1
karlos g liberal <karlosgliberal@gmail.com>
2013-04-29 19:46:24 +0200

    Cambio en el la configuración de grunt para para que mitifique el app.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   // Initializes the Grunt tasks with the following settings
   grunt.initConfig({
-      // Project configuration.
+    // Project configuration.
     // Files to be concatenated … (source and destination files)
     concat: {
       css: {
@@ -10,21 +10,27 @@ module.exports = function(grunt) {
         dest: 'public/css/screen.css'
       }
     },
-  cssmin: {
-      dist:{
-        src:['<%= concat.css.dest  %>'],
-        dest: 'public/css/screen.min.css'
-      }
+    cssmin: {
+        dist:{
+          src:['<%= concat.css.dest  %>'],
+          dest: 'public/css/screen.min.css'
+        }
     },
-
+    uglify: {
+      dist: {
+          src: ['public/js/app.js'],
+          dest: 'public/js/app.min.js'
+      }
+    }
   });
 
   // Load the plugins that provide the tasks we specified in package.json.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   // This is the default task being executed if Grunt
   // is called without any further parameter.
-  grunt.registerTask('default', ['concat', 'cssmin']);
+  grunt.registerTask('default', ['concat', 'cssmin', 'uglify']);
 
 };
 
