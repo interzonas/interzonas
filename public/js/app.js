e8d3bf9841f6db76d7da6679ec7129f103696801
karlos g liberal <karlosgliberal@gmail.com>
2013-06-12 22:05:38 +0200

    Footer effect

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   //Efecto proyectos
   var cancel;
-  var container = document.getElementById('container');
+  //var container = document.getElementById('container');
   var slide = document.getElementById('slide2');
   var renderer = new FSS.CanvasRenderer();
   var scene = new FSS.Scene();
@@ -19,10 +19,6 @@ $(document).ready(function(){
     renderer.setSize(slide.offsetWidth, slide.offsetHeight);
     light.setPosition(10,10,30);
     renderer.render(scene);
-  }
-
-  function resize() {
-    renderer.setSize(slide.offsetWidth, slide.offsetHeight);
   }
 
   function animate() {
@@ -146,4 +142,223 @@ $(document).ready(function(){
   });
   
   fondoinst(); 
+
+
+
+      var stage = document.getElementById('stage');
+      
+      var color = "#" + genHex();
+      var springs = [];
+      var storeY;
+      var extend = 100;
+      var fK = .95;
+      var particles = [];
+      var currentDrag = null;
+      var mouseX = 0;
+      var mouseY = 0;
+      var stageWidth = $('#footer').width();
+      var stageHeight = $('#footer').height();
+      stage.width = stageWidth;
+      stage.height = stageHeight;
+      
+      var IE = document.all ? true : false;
+      if(!IE) document.addEventListener(Event.MOUSEMOVE, getMouseXY, false);
+      $("section#footer").mouseenter(function(){
+        document.onmousemove = getMouseXY;
+      }).mouseleave(function(){
+      });
+      
+      
+      generate();
+      
+      var drawingCanvas = document.getElementById('stage');
+      if(drawingCanvas.getContext)
+      {
+        var context = drawingCanvas.getContext('2d');
+        setInterval(render, 20);
+      }
+      
+      
+      function genHex()
+      {
+        colors = new Array(14)
+        colors[0]="0"
+        colors[1]="1"
+        colors[2]="2"
+        colors[3]="3"
+        colors[4]="4"
+        colors[5]="5"
+        colors[5]="6"
+        colors[6]="7"
+        colors[7]="8"
+        colors[8]="9"
+        colors[9]="a"
+        colors[10]="b"
+        colors[11]="c"
+        colors[12]="d"
+        colors[13]="e"
+        colors[14]="f"
+        
+        digit = new Array(5)
+        color=""
+        for (i=0;i<6;i++)
+        {
+          digit[i]=colors[Math.round(Math.random()*14)]
+          color = color+digit[i]
+        }
+        
+        return '00a3d9';
+      }
+      
+      
+      function generate()
+      {
+        var total = Math.ceil(stageWidth / 25);
+        springs = [];
+        particles = [];
+        
+        var space = (stageWidth + extend) / total;
+        var xpos = (space * .5) - (extend * .5);
+        var ypos = stageHeight * .5;
+        
+        for(var i = 0; i < total; i++)
+        {
+          var particle = {};
+          particle.x = particle.xpos = xpos;
+          particle.y = particle.ypos = particle.origY = ypos;
+          particle.ay = 0;
+          particle.vy = 0;
+          particle.mass = 10;
+          particles[particles.length] = particle;
+          
+          xpos += space;
+        }
+        
+        
+        storeY = mouseY;
+        for(var u = 0; u < particles.length-1; u++) springs.push({iLengthY:(particles[u+1].y - particles[u].y)});
+      }
+      
+      
+      function mouseMove()
+      {
+        var particle = null;
+        var smallestDist = Infinity;
+        var target = null;
+        
+        var j = particles.length;
+        while(--j > -1)
+        {
+          var dx = mouseX - particles[j].x;
+          var dy = mouseY - particles[j].y;
+          var dist = Math.sqrt(dx * dx + dy * dy);
+          
+          if(dist < smallestDist)
+          {
+            particle = particles[j];
+            smallestDist = dist;
+            target = j;
+          }
+        }
+        
+        if(particle && mouseY > particle.y)
+        {
+          var speed = mouseY - storeY;
+          
+          particles[target - 2].vy = speed / 6;
+          particles[target - 1].vy = speed / 5;
+          particles[target].vy = speed / 3;
+          particles[target + 1].vy = speed / 5;
+          particles[target + 2].vy = speed / 6;
+        
+          storeY = mouseY;
+        }
+      }
+      
+      
+      function render()
+      {
+        for(var u = particles.length-1; u >= 0; --u)
+        {
+          var fExtensionY = 0;
+          var fForceY = 0;
+        
+          if(u > 0)
+          {
+            fExtensionY = particles[u-1].y - particles[u].y - springs[u-1].iLengthY;
+            fForceY += -fK * fExtensionY;
+          }
+          
+          if(u < particles.length-1)
+          {
+            fExtensionY = particles[u].y - particles[u+1].y - springs[u].iLengthY;
+            fForceY += fK * fExtensionY;
+          }
+          
+          fExtensionY = particles[u].y - particles[u].origY;
+          fForceY += fK/15 * fExtensionY;
+          
+          particles[u].ay = -fForceY/particles[u].mass;
+          particles[u].vy += particles[u].ay;
+          particles[u].ypos += particles[u].vy;
+          particles[u].vy /= 1.04;
+        }
+        
+        context.clearRect(0, 0, stageWidth, stageHeight);
+        context.fillStyle = color;
+        context.beginPath();
+        
+        for(u = 0; u < particles.length; u++)
+        {
+          if(u==0)
+          {
+            context.moveTo(particles[u].xpos+(particles[u+1].xpos-particles[u].xpos)/2,particles[u].ypos+(particles[u+1].ypos-particles[u].ypos)/2);
+          }
+          else if(u < particles.length-1)
+          {
+            context.quadraticCurveTo(particles[u].xpos,particles[u].ypos,particles[u].xpos+(particles[u+1].xpos-particles[u].xpos)/2,particles[u].ypos+(particles[u+1].ypos-particles[u].ypos)/2);
+          }
+          particles[u].x = particles[u].xpos;
+          particles[u].y = particles[u].ypos;
+        }
+        
+        context.lineTo(stageWidth+50, stageHeight+50);
+        context.lineTo(-50, stageHeight+50);
+        context.lineTo(-50, stageHeight/2);
+        context.closePath();
+        context.fill();
+        
+        /*var i = particles.length;
+        while(--i > -1)
+        {
+          context.fillStyle = "#000000";
+          context.beginPath();
+          context.arc(particles[i].x,particles[i].y,1,0,Math.PI*2,true);
+          context.closePath();
+          context.fill();
+
+        }*/
+      }
+      
+      function getMouseXY(e)
+      {
+          if(IE)
+          {
+            mouseX = event.clientX + document.body.scrollLeft
+            mouseY = event.clientY + document.body.scrollTop
+          }
+          else
+          {
+            mouseX = e.pageX
+            mouseY = e.pageY
+          }  
+          
+          if(mouseX < 0) {mouseX = 0;}
+          if(mouseY < 0) {mouseY = 0;}  
+          
+          mouseMove();
+          
+          return true;
+      }
+
 })
